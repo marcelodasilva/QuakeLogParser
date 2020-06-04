5a2ebd851d19b8b6946b7237b5cfbe9aebcf7b17
Marcelo da Silva <marcelosilva.mdc@gmail.com>
2020-06-04 02:00:52 -0300

    test: Read file and lines of log

--- __tests__/unit/parser.spec.js
+++ __tests__/unit/parser.spec.js
@@ -1,6 +1,24 @@
+import path from 'path'
+import { readLog,processLineKills } from "../../src/app/Parser";
+
+
 describe("Parser", ()=> {
-  it('be able of run', () => {
-    expect(2 + 2).toBe(4)
+  it('should be able read the game.log file', async () => {
+
+   const result = readLog(path.resolve(__dirname,'../games.log'))
+
+   expect(result).toHaveLength(5306)
   })
+
+  it('should be able read lines of game.log', async () => {
+   const lines = readLog(path.resolve(__dirname,'../games.log'))
+
+
+ expect(lines[5262]).toEqual(' 13:27 Kill: 6 5 7: Zeh killed Assasinu Credi by MOD_ROCKET_SPLASH')
+   
+
+  })
+
+ 
   
 })
